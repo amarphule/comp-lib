4d2c772b287bb74ca80766cfd511ef7d742bfe49
Amardeep Phule <amardeepphule@gmail.com>
2022-04-08 22:21:54 +0530

    fix: suggested changes

--- js/modal.js
+++ js/modal.js
@@ -1,14 +1,17 @@
-let action = document.querySelector(".action")
-let modal = document.querySelector(".modal")
-let close = document.querySelector(".close")
-action.addEventListener('click', ()=>{
-    modal.style.display = "block"
-    //On click anywhere Outside modal will close it
-    window.addEventListener('click',(e)=>{
-        if(e.target==modal ){
-            modal.style.display = "none"
-        }
-    })
-})
+let action = document.querySelector(".action");
+let modal = document.querySelector(".modal");
+let close = document.querySelector(".close");
+let closebtn = document.querySelector("#close");
 
-
+// closebtn.addEventListener("click", () => {
+//   //   modal.style.display = "none";
+// });
+action.addEventListener("click", () => {
+  modal.style.display = "block";
+  //On click anywhere Outside modal will close it
+  window.addEventListener("click", (e) => {
+    if (e.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+});
